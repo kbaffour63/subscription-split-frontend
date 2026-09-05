28883892fb31d1236a1de54604ba096d209d3eee
kb603 <kojoaddobaffour@gmail.com>
2026-09-05 14:41:08 +0000

    updates

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
   timeout: 5000,
   headers: { "Content-Type": "application/json" },
 });
